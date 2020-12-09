8bdf366dc224422980f548fe305e80ba746328d7
Andrew Poppenberg <andrewpoppenberg@yahoo.com>
2020-12-09 15:54:42 -0600

    finished mvp

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,36 @@
 // scheme-model
+const db = require('../../data/db-config')
+
+function find() {
+    return db('schemes')
+}
+function findById(id) {
+    return db('schemes as s').where('s.id', id)
+
+}
+function findSteps() {
+    return db('schemes as sc')
+        .join('steps as st', 'sc.id', 'st.scheme_id')
+        .select('st.instructions', 'st.step_number')
+        .where('sc.id', id)
+}
+function add(scheme) {
+    return db('schemes').insert(scheme)
+}
+
+function update(changes, id) {
+    return db('schemes as s')
+        .where('s.id', id)
+        .update('scheme_name', changes.scheme_name)
+}
+function remove() {
+    return db('schemes as s').where('s.id', id).del()
+}
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
